093b575675f345592558f0c7546eb614a7d6dc72
shibaiyong <shibaiyong@example.com>
2019-09-17 09:12:44 +0800

    修改api

--- src/assets/utils/api.js
+++ src/assets/utils/api.js
@@ -3,17 +3,17 @@ import { filters } from "./index"
 export default{  
   install (Vue, options) {
     Vue.directive('has', {
-        inserted(el, binding, vnode) {
-          // 获取按钮权限
-          let { add, edit, remove } = vnode.context.$route.query
-          if(binding.value == 'add'&& add == 0){
-            el.parentNode.removeChild(el)
-          }else if(binding.value == 'edit'&& edit == 0){
-            el.parentNode.removeChild(el)
-          }else if(binding.value == 'remove'&& remove == 0){
-            el.parentNode.removeChild(el)
-          }
+      inserted(el, binding, vnode) {
+        // 获取按钮权限
+        let { add, edit, remove } = vnode.context.$route.query
+        if(binding.value == 'add'&& add == 0){
+          el.parentNode.removeChild(el)
+        }else if(binding.value == 'edit'&& edit == 0){
+          el.parentNode.removeChild(el)
+        }else if(binding.value == 'remove'&& remove == 0){
+          el.parentNode.removeChild(el)
         }
+      }
     })
     Object.keys( filters ).forEach(key => {
       Vue.filter(key, filters[key])
